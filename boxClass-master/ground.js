f1ce070e49608565964a3b810d4f8c476b306436
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-22 22:50:09 +0530

    Update ground.js

--- boxClass-master/ground.js
+++ boxClass-master/ground.js
@@ -1,13 +1,12 @@
 class Ground {
-    constructor(x,y,width,height){
+    constructor(x,y,width,height) {
         var options = {
             isStatic: true
         }
-    
-    this.body = Bodies.rectangle(x,y,width,height,options);
+        this.body = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
     this.height = height;
-    World.add(world,ground);
+    World.add(world, thisbody);
 
 }
 display(){
