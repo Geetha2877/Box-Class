b0f9abb41856a91687143f799a40ad9db0bf07fa
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-22 22:51:34 +0530

    Update ground.js

--- boxClass-master/ground.js
+++ boxClass-master/ground.js
@@ -6,8 +6,7 @@ class Ground {
         this.body = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
     this.height = height;
-    World.add(world, thisbody);
-
+    World.add(world, this.body);
 }
 display(){
     var position = this.body.position;
